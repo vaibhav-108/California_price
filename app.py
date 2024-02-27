da24a79a1390e7ece7d18fa6e3fb75107fec011a
vaibhav-108 <vaibhav.b108@gmail.com>
2024-02-27 15:20:42 +0100

    web app redy

--- app.py
+++ app.py
@@ -15,15 +15,26 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict_api',methods=['POST'])
-def predict():
+def predict_api():
     data = request.json["data"]
     print(f" inserted data {data}")
     print(f" correct data {np.array(list(data.values())).reshape(1,-1)}")
     new_data= scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
-    
     return jsonify (output[0])
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    print('Form data')
+    
+    new_data = scalar.transform(np.array(data).reshape(1,-1))
+    output =  regmodel.predict(new_data)
+    print(output)
+    
+    return render_template('home.html', predict_value = 'The predict value is {}'.format(round(output[0],2)))
     
     
 if __name__ == "__main__":
